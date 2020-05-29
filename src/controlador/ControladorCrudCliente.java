8a044d7392ef46e97fb77eab113c89f32ee35e2c
Brahian <Brahian@Brahian-PC>
2020-05-29 10:02:28 -0500

    controlador de crud casi terminado

--- src/controlador/ControladorCrudCliente.java
+++ src/controlador/ControladorCrudCliente.java
@@ -5,11 +5,13 @@ import excepciones.ClienteNoExisteException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import modelo.Cliente;
+
 
 public class ControladorCrudCliente implements ControladorVentana {
 
@@ -20,19 +22,52 @@ public class ControladorCrudCliente implements ControladorVentana {
 	@FXML
 	private TableColumn<Cliente, String> nombreColumn;
 	@FXML
-	private TableColumn<Cliente, String> apellidoColumn;
+	private TableColumn<Cliente, String> tipoDocumentoColumn;
 	@FXML
-	private TableColumn<Cliente, String> cedulaColumn;
+	private TableColumn<Cliente, String> numeroDColumn;
+	@FXML
+	private TableColumn<Cliente, String> telefonoColumn;
+	@FXML
+	private TableColumn<Cliente, String> celularColumn;
+	
 	@FXML
 	private TableColumn<Cliente, String> direccionColumn;
 	@FXML
+	private TableColumn<Cliente, String> ciudadRColumn;
+	@FXML
+	private TableColumn<Cliente, String> departamentoColumn;
+	@FXML
+	private TableColumn<Cliente, String> paisColumn;
+	@FXML
+	private TableColumn<Cliente, String> profesionColumn;
+	@FXML
+	private TableColumn<Cliente, String> emailColumn;
+	@FXML
 	private TextField nombreText;
 	@FXML
-	private TextField apellidoText;
+	private ComboBox<String> tipoDocumentoText;
 	@FXML
-	private TextField cedulaText;
+	private TextField numeroDText;
+	@FXML
+	private TextField telefonoText;
+	@FXML
+	private TextField  celularText;
 	@FXML
 	private TextField direccionTest;
+	@FXML
+	private TextField ciudadRText;
+	@FXML
+	private TextField departamentoText;
+	@FXML
+	private TextField  paisText;
+	@FXML
+	private TextField profesionText;
+	@FXML
+	private TextField emailText;
+	
+	
+	
+	
 	private boolean nuevo = true;
 
 	@FXML
@@ -41,9 +76,15 @@ public class ControladorCrudCliente implements ControladorVentana {
 		ControladorPrincipal.registrarControladorVentana(this);
 
 		nombreColumn.setCellValueFactory(new PropertyValueFactory<Cliente, String>("nombre"));
-		apellidoColumn.setCellValueFactory(new PropertyValueFactory<Cliente, String>("apellido"));
-		cedulaColumn.setCellValueFactory(new PropertyValueFactory<Cliente, String>("cedula"));
+		tipoDocumentoColumn.setCellValueFactory(new PropertyValueFactory<Cliente, String>("tipoDocumento"));
+		numeroDColumn.setCellValueFactory(new PropertyValueFactory<Cliente, String>("numeroDocumento"));
+		telefonoColumn.setCellValueFactory(new PropertyValueFactory<Cliente, String>("telefono"));
+		celularColumn.setCellValueFactory(new PropertyValueFactory<Cliente, String>("celular"));
 		direccionColumn.setCellValueFactory(new PropertyValueFactory<Cliente, String>("Direccion"));
+		departamentoColumn.setCellValueFactory(new PropertyValueFactory<Cliente, String>("departamento"));
+		paisColumn.setCellValueFactory(new PropertyValueFactory<Cliente, String>("pais"));
+	    profesionColumn.setCellValueFactory(new PropertyValueFactory<Cliente, String>("profesion"));
+	    emailColumn.setCellValueFactory(new PropertyValueFactory<Cliente, String>("email"));
 		clienteData = FXCollections.observableArrayList(ControladorEmpresa.getInstance().listarClientles());
 		tablaClientes.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
 			showClientDetails(newValue);
@@ -64,18 +105,39 @@ public class ControladorCrudCliente implements ControladorVentana {
 	private void showClientDetails(Cliente cliente) {
 		if (cliente != null) {
 			nombreText.setText(cliente.getNombre());
-			apellidoText.setText(cliente.getApellido());
-			cedulaText.setText(cliente.getCedula());
+			tipoDocumentoText.setValue(cliente.getTipoDocumento());
+			numeroDText.setText(cliente.getNumeroDocumento());
+			telefonoText.setText(cliente.getTelefonoResidencia());
+			celularText.setText(cliente.getTelefonoCelular());
 			direccionTest.setText(cliente.getDireccion());
+			ciudadRText.setText(cliente.getCiudadR());
+			departamentoText.setText(cliente.getDepartamento());
+			paisText.setText(cliente.getPais());
+			profesionText.setText(cliente.getProfesion());
+			emailText.setText(cliente.getEmail());
+			
+			
 		} else {
-			nombreText.setText("");
-			apellidoText.setText("");
+		    tipoDocumentoList.getSelectionModel().clearSelection();
 			cedulaText.setText("");
-			direccionTest.setText("");
+			nombreText.setText("");
+			direccionText.setText("");
+			telefonoText.setText("");
+			celularText.setText("");
+			ciudadText.setText("");
+			departamentoText.setText("");
+			paisText.setText("");
+			emailText.setText("");
+			profesionText.setText("");
+			
 			nuevo = true;
 			tablaClientes.getSelectionModel().clearSelection();
+			
 		}
+		
+		tablaClientes.refresh();
 	}
+	
 
 	@FXML
 	public void agregarCliente() {
